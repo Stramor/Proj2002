f474afe1ff4710751ee0f50bf431594d16249081
Alexander Kobelev <ak.mail.ru@gmail.com>
2013-02-21 11:25:23 +0400

    Simplify code and fix NumberFormatException

--- src/com/stramor/tryto/Graph.java
+++ src/com/stramor/tryto/Graph.java
@@ -1,5 +1,13 @@
 package com.stramor.tryto;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -7,50 +15,63 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
-import java.io.*;
-import javax.swing.*;
 
 public class Graph {
+
   public static void main(String[] args) {
-    File file = new File("011.txt");
-    StringBuilder contents = new StringBuilder();
-    BufferedReader reader = null;
+    String content = readFile("01.txt");
 
-    try {
-      reader = new BufferedReader(new FileReader(file));
-      String text = null;
-      while ((text = reader.readLine())!=null) {
-        contents.append(text).append(System.getProperty("line.separator"));
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }finally {
-      try {
-        if (reader != null) {reader.close();}
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-    String[] dataStr = contents.toString().split("\\s+");
-    System.out.println(contents.toString());
 
-    double numb = 0;
+    String[] dataStr = content.split("\\s+");
+    System.out.println(content);
+
+    double numb;
 
     XYSeries series = new XYSeries("sin(a)");
     for (int i = 0; i < dataStr.length; i++) {
-      numb = Double.parseDouble(dataStr[i]);
-      System.out.println(numb);
-      series.add(i, numb);
-      //series.add(i, Math.cos(i));
+      if (dataStr[i] != null && !dataStr[i].isEmpty()) {
+        numb = Double.parseDouble(dataStr[i]);
+        System.out.println(numb);
+        series.add(i, numb);
+        //series.add(i, Math.cos(i));
+      }
     }
     System.out.println(dataStr.length);
 
     XYDataset xyDataset = new XYSeriesCollection(series);
-    JFreeChart chart = ChartFactory.createXYLineChart("", "i","mV", xyDataset, PlotOrientation.VERTICAL, false, true, true);
+    JFreeChart chart = ChartFactory.createXYLineChart("", "i", "mV", xyDataset, PlotOrientation.VERTICAL, false, true, true);
     JFrame frame = new JFrame("MinimalStaticChart");
     frame.getContentPane().add(new ChartPanel(chart));
-    frame.setSize(800,500);
+    frame.setSize(800, 500);
     frame.setVisible(true);
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
   }
 
+  private static String readFile(String fileName) {
+    File file = new File(fileName);
+    StringBuilder stringBuilder = new StringBuilder();
+    BufferedReader reader = null;
+
+    try {
+      reader = new BufferedReader(new FileReader(file));
+      String text;
+      while ((text = reader.readLine()) != null) {
+        stringBuilder.append(text).append(System.getProperty("line.separator"));
+      }
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    finally {
+      try {
+        if (reader != null) {
+          reader.close();
+        }
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return stringBuilder.toString();
+  }
 }
